a932797e6cc91d6b4b807b8e111e25ec1325fd98
ye ji <dpwl9435@gmail.com>
2024-08-29 22:42:12 +0900

    최초 커밋

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import path from "path";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src", "styles")],
+  },
+};
 
 export default nextConfig;
