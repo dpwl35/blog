ad504ca133450b066d590e6bcd5410c10f2eb54f
ye ji <dpwl9435@gmail.com>
2024-09-11 21:18:03 +0900

    🎨 코드 수정 / 텍스트 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
     <section className='main-content'>
-      <div className='main-content-area'>기록용 블로그 입니다.</div>
+      <div className='main-content-area'>
+        <p>
+          인터랙티브 요소를 통해 사용자가 더욱 몰입할 수 있는 경험을 만드는 데 관심이 많습니다.
+          <br />
+          이곳은 틈틈이 배우고 익힌 것들을 기록하는 공간입니다.
+        </p>
+      </div>
     </section>
   );
 }
