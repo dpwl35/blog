5d4c53aa5ad82b9c293382f5bf0e29344edb011b
ye ji <dpwl9435@gmail.com>
2024-09-26 23:29:34 +0900

    🎨 Art : test

---
+++ src/posts/archive/2024/sample/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'ddd1111d',
+  description: 'ddddd1111111',
+};
+
+const SamplePost: React.FC = () => {
+  return <div>Sample Content</div>;
+};
+
+export default SamplePost;
---
+++ src/posts/archive/2024/test/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'dddd',
+  description: 'ddddd',
+};
+
+const Test: React.FC = () => {
+  return <div>Test Content</div>;
+};
+
+export default Test;
