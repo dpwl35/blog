f5e5406d0d4e0328a3e4cb0c38a1b1701d9690f9
dpwl35 <dpwl9435@gmail.com>
2025-06-27 23:05:08 +1000

    feat: a태그 제거

--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -66,24 +66,12 @@ function slugify(str: string) {
 
 function createHeading(level) {
   const Heading = ({ children }) => {
-    let slug = slugify(children);
+    const slug = slugify(children);
 
-    return React.createElement(
-      `h${level}`,
-      { id: slug },
-      [
-        React.createElement("a", {
-          href: `#${slug}`,
-          key: `link-${slug}`,
-          className: "anchor",
-        }),
-      ],
-      children
-    );
+    return React.createElement(`h${level}`, { id: slug }, children);
   };
 
   Heading.displayName = `Heading${level}`;
-
   return Heading;
 }
 
