ecc40f91576d5fd5cc030bc1bf64dfa88fc3bf8b
dpwl35 <dpwl9435@gmail.com>
2025-09-15 23:41:01 +1000

    feat: room 추가

---
+++ lab/room.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import {
+  OrbitControls,
+  useHelper,
+  useGLTF,
+  Environment,
+} from "@react-three/drei";
+import { Suspense, useRef } from "react";
+
+const Scene = () => {
+  const { scene } = useGLTF("/models/room.glb");
+  return <primitive object={scene} />;
+};
+useGLTF.preload("/models/room.glb");
+
+const Light = () => {
+  const directionalLightRef = useRef<THREE.DirectionalLight>(null);
+
+  useHelper(directionalLightRef, THREE.DirectionalLightHelper, 2, "red");
+
+  return (
+    <>
+      <ambientLight intensity={2} color={0xffffff} />
+      <directionalLight
+        ref={directionalLightRef}
+        position={[2, 10, 0]}
+        color={0xffffff}
+        intensity={15}
+        castShadow
+      />
+    </>
+  );
+};
+
+export default function Room({ className }: { className?: string }) {
+  return (
+    <Canvas camera={{ position: [20, 0, 0], fov: 50 }}>
+      <color attach="background" args={["#ffffff"]} />
+      <Light />
+      <Scene />
+      {/* <OrbitControls
+        makeDefault
+        target={[-0.5, 2, 0]}
+        enablePan={false}
+        minAzimuthAngle={-Math.PI / 2}
+        maxAzimuthAngle={Math.PI / 3}
+        minPolarAngle={Math.PI / -1}
+        maxPolarAngle={Math.PI / 3}
+        maxDistance={15}
+        minDistance={2}
+      /> */}
+      <OrbitControls makeDefault target={[-0.5, 2, 0]} />
+    </Canvas>
+  );
+}
