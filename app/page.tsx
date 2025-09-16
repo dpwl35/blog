4146eb05cc9c41a4167a0df56359feefe0a7a4bc
dpwl35 <dpwl9435@gmail.com>
2025-09-16 15:18:12 +1000

    feat: 수정

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Room from "lab/room";
 export default function Page() {
   return (
     <section className="main-content">
-      <Room />
+      {/* <Room /> */}
       <p className="main-content-area">{`안녕하세요.`}</p>
       {/* <div>{<BlogPosts />}</div> */}
     </section>
