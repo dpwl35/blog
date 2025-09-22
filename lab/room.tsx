bff6f9c8c58f4fb01e3f032875022e7b04eb9b9e
dpwl35 <dpwl9435@gmail.com>
2025-09-22 22:39:00 +1000

    feat: 낮,밤 조명 추가

--- lab/room.tsx
+++ lab/room.tsx
@@ -7,8 +7,27 @@ import {
   useHelper,
   useGLTF,
   Environment,
+  Html,
 } from "@react-three/drei";
-import { Suspense, useEffect, useRef } from "react";
+import { Suspense, useEffect, useRef, useState } from "react";
+
+const glassMaterial = new THREE.MeshPhysicalMaterial({
+  transmission: 0.5,
+  opacity: 1,
+  roughness: 0.1,
+  metalness: 0,
+  ior: 1.5,
+  thickness: 0.02,
+  specularIntensity: 1,
+  envMapIntensity: 1,
+  clearcoat: 1,
+  clearcoatRoughness: 0,
+});
+const waxMaterial = new THREE.MeshPhysicalMaterial({
+  color: new THREE.Color("#fff4e6"),
+  roughness: 0.8,
+  metalness: 0,
+});
 
 const Scene = () => {
   const { scene } = useGLTF("/models/room.glb");
@@ -16,36 +35,22 @@ const Scene = () => {
   useEffect(() => {
     scene.traverse((child: any) => {
       if (child.isMesh) {
+        child.castShadow = true;
+        child.receiveShadow = true;
+
         const name = child.name.toLowerCase();
 
-        // 유리
         if (name === "candle") {
-          console.log("✅ Candle Mesh 적용:", child.name);
-
-          child.material = new THREE.MeshPhysicalMaterial({
-            transmission: 0.5,
-            opacity: 1,
-            roughness: 0.1,
-            metalness: 0,
-            ior: 1.5,
-            thickness: 0.02,
-            specularIntensity: 1,
-            envMapIntensity: 1,
-            clearcoat: 1,
-            clearcoatRoughness: 0,
-          });
+          child.material = glassMaterial;
         }
 
-        // 왁스
         if (name.includes("candle001")) {
-          console.log("Wax 적용:", child.name);
-
-          child.material = new THREE.MeshStandardMaterial({
-            color: new THREE.Color("#fff4e6"), // 아이보리/왁스 컬러
-            roughness: 0.8,
-            metalness: 0,
-          });
+          child.material = waxMaterial;
         }
+
+        // if (name.includes("floor")) {
+        //   child.receiveShadow = true;
+        // }
       }
     });
   }, [scene]);
@@ -54,31 +59,86 @@ const Scene = () => {
 };
 useGLTF.preload("/models/room.glb");
 
-const Light = () => {
+const Light = ({ isNight }: { isNight: boolean }) => {
   const directionalLightRef = useRef<THREE.DirectionalLight>(null);
-
   useHelper(directionalLightRef, THREE.DirectionalLightHelper, 2, "red");
+  const pointLightRef = useRef<THREE.PointLight>(null);
+  useHelper(pointLightRef, THREE.PointLightHelper, 0.5, "blue");
+  const spotLightRef = useRef<THREE.SpotLight>(null);
+  useHelper(spotLightRef, THREE.SpotLightHelper, "red");
 
   return (
     <>
-      <ambientLight intensity={2} color={0xffffff} />
-      <directionalLight
-        ref={directionalLightRef}
-        position={[2, 10, 0]}
-        color={0xffffff}
-        intensity={15}
-        castShadow
-      />
+      {isNight ? (
+        <>
+          <ambientLight intensity={0.2} color={0x222244} />
+          <directionalLight
+            // ref={directionalLightRef}
+            position={[3, 10, -5]}
+            color={0x88aaff}
+            intensity={0.5}
+            castShadow
+          />
+          <pointLight
+            // ref={pointLightRef}
+            position={[-2.9, 3.5, -2.2]}
+            intensity={10}
+            distance={50}
+            decay={10}
+            castShadow
+            color={"#ffffff"}
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+          />
+          <pointLight
+            // ref={pointLightRef}
+            position={[-2.1, 5, 0.1]}
+            intensity={30}
+            distance={7}
+            decay={2}
+            castShadow
+            color={"#ffffff"}
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+          />
+          {/* <spotLight
+            ref={spotLightRef}
+            position={[-2.3, 5.5, 0.1]} // 스탠드 위치
+            angle={0.5} // 조금 넓게
+            penumbra={0.6} // 가장자리 부드럽게
+            intensity={150} // 충분히 밝게
+            distance={8} // 책상 높이에 닿도록
+            decay={1.5} // 감쇠 완화
+            color={"#fff5cc"} // 전구 색
+            castShadow
+          /> */}
+        </>
+      ) : (
+        <>
+          <ambientLight intensity={2} color={0xffffff} />
+          <directionalLight
+            // ref={directionalLightRef}
+            position={[3, 10, -5]}
+            color={0xffffff}
+            intensity={15}
+            castShadow
+          />
+        </>
+      )}
     </>
   );
 };
 
 export default function Room({ className }: { className?: string }) {
+  const [isNight, setIsNight] = useState(true);
+
   return (
-    <Canvas camera={{ position: [20, 20, 20], fov: 20 }}>
-      <color attach="background" args={["#ffffff"]} />
-      <Environment preset="city" />
-      <Light />
+    <Canvas shadows camera={{ position: [20, 20, 20], fov: 20 }}>
+      <color attach="background" args={[isNight ? "#000000" : "#ffffff"]} />
+
+      {!isNight && <Environment preset="city" />}
+
+      <Light isNight={isNight} />
       <Scene />
       {/* <OrbitControls
         makeDefault
@@ -90,6 +150,24 @@ export default function Room({ className }: { className?: string }) {
         minDistance={2}
       /> */}
       <OrbitControls makeDefault target={[-0.5, 2, 0]} />
+
+      {/* Canvas 안에서 버튼 추가 */}
+      <Html position={[-2, 8, 0]}>
+        <button
+          onClick={() => setIsNight((prev) => !prev)}
+          style={{
+            padding: "8px 12px",
+            background: "#333",
+            color: "#fff",
+            border: "none",
+            borderRadius: "6px",
+            cursor: "pointer",
+            whiteSpace: "nowrap",
+          }}
+        >
+          {isNight ? "☀️ 낮으로" : "🌙 밤으로"}
+        </button>
+      </Html>
     </Canvas>
   );
 }
