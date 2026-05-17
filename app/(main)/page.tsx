94875ee776b03d8ea2d049a9db12d7f7a1dbd252
dpwl35 <dpwl9435@gmail.com>
2026-05-17 22:55:22 +0900

    feat: 변경

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,15 +1,9 @@
 import dynamic from "next/dynamic";
 
-const Room = dynamic(() => import("lab/room"), {
-  ssr: false,
-  loading: () => <div>loading...</div>,
-});
-
 export default function Page() {
   return (
     <section className="main-content">
       <div className="main-content-area">
-        <Room transparent />
         <p>{`안녕하세요.`}</p>
       </div>
     </section>
