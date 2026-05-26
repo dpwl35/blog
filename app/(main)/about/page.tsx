f85bd49ad154e5c751460d17cc57923e1b6a9d88
dpwl35 <dpwl9435@gmail.com>
2026-05-26 21:32:36 +0900

    feat: about 페이지 추가

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -2,6 +2,43 @@ export default function AboutPage() {
   return (
     <div className="post-section">
       <div className="about">
+        <div className="about-intro">
+          <p>
+            견고한 마크업으로 정보와 가치를 전달하는 웹 퍼블리셔 김예지입니다.
+          </p>
+          <p>
+            웹 구현의 매력에 이끌려 퍼블리셔로 시작해, 지금은 어디서든 잘 보이는 반응형 웹과 웹 표준·접근성을 고려한 견고한 UI를 만드는 일을 하고 있습니다. 어떤 디바이스와 브라우저에서도 일관된 화면을 제공하는 것을 기본으로, 맡은 프로젝트의 목적을 먼저 이해하고 정보와 가치가 사용자에게 잘 전달되는 구조를 고민합니다.
+          </p>
+          <p>
+            단순히 화면을 옮기는 것이 아니라, 다양한 직군과 원활하게 소통하며 협업하는 것을 중요하게 생각하고, 함께 멋지고 즐거운 UX를 만드는 과정을 즐깁니다. 부족한 부분은 공부로 채우고, 더 나은 방식을 끊임없이 탐구하며 성장하고 있습니다.
+          </p>
+        </div>
+        <ul className="about-career">
+          <li className="about-career-item">
+            <p>ᴡᴏʀᴋ ᴇxᴘᴇʀɪᴇɴᴄᴇ</p>
+          </li>
+          <li className="about-career-item">
+            <div>
+              <span>프리랜서</span>
+              <span>2025.01 - 2026.01</span>
+            </div>
+            <div> 경력사항 </div>
+          </li>
+          <li>
+            <div>
+              <span>코나아이</span>
+              <span>2024.01 - 2025.01</span>
+            </div>
+            <div>에코시안 경력사항 </div>
+          </li>
+          <li>
+            <div>
+              <span>에코시안</span>
+              <span>2020.9 - 2023.09 </span>
+            </div>
+            <div>에코시안 경력사항 </div>
+          </li>
+        </ul>
         <div>자기소개 더 보기</div>
       </div>
     </div>
