e2856c87f63c5f0322ee1182b6d792ec9136b9cc
dpwl35 <dpwl9435@gmail.com>
2026-05-29 00:23:00 +0900

    feat: 최신 글 목록 추가

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,8 +1,9 @@
 import { getBlogPosts, formatDate } from "app/(main)/blog/utils";
 import { getLabItems } from "../lab/utils";
 import Link from "next/link";
+import MainEvent from "app/components/mina-event";
 
-export default async function Page() {
+export default async function MainPage() {
   const posts = getBlogPosts()
     .sort(
       (a, b) =>
@@ -15,36 +16,42 @@ export default async function Page() {
 
   return (
     <div className="main-content">
-      <div className="main-content-area">
-        <p>{`안녕하세요.`}</p>
-      </div>
-      <div>
-        <div>
-          <p>BLOG : 인사이트를 보관하는 곳</p>
-          <ul>
+      <MainEvent />
+      <div className="main-feed">
+        <div className="main-feed-area">
+          <p className="main-feed-title">BLOG : 기록 보관소</p>
+          <ul className="main-feed-iist">
             {posts.map((post) => (
-              <li key={post.slug}>
-                <Link href={`/blog/${post.slug}`}>{post.metadata.title}</Link>
-                <span>{formatDate(post.metadata.publishedAt)}</span>
+              <li key={post.slug} className="main-feed-item">
+                <Link href={`/blog/${post.slug}`} className="main-feed-link">
+                  {post.metadata.title}
+                </Link>
+                <span className="main-feed-date">
+                  {formatDate(post.metadata.publishedAt)}
+                </span>
               </li>
             ))}
           </ul>
         </div>
-        <div>
-          <p>AECHIVE : 흥미있는 것을 만드는 공간</p>
-          <ul>
+        <div className="main-feed-area">
+          <p className="main-feed-title">AECHIVE : 작업물 모음</p>
+          <ul className="main-feed-iist">
             {labItems.map((item) => (
-              <li key={item.slug}>
+              <li key={item.slug} className="main-feed-item">
                 <Link
                   href={`/lab/${item.slug}`}
                   target="_blank"
                   rel="noopener noreferrer"
+                  className="main-feed-link"
                 >
                   {item.title}
                 </Link>
               </li>
             ))}
           </ul>
+        </div>
+        <div className="main-feed-area">
+          <p className="main-feed-title">NOTES : 학습 기록장</p>
         </div>
       </div>
     </div>
